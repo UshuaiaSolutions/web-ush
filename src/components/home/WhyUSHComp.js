2677800e98322bb9df5ad2b9185acb6f74aedc5e
UshuaiaSolutions <creativo@ushuaiasolutions.com>
2023-09-09 20:44:46 -0300

    home: why ush

--- src/components/home/WhyUSHComp.js
+++ src/components/home/WhyUSHComp.js
@@ -2,52 +2,79 @@ import styled from "styled-components";
 import { breakpoints } from "../base/breakpoints";
 
 export const StyledContainer = styled.div`
-  padding: 60px 0;
+  padding: 56px 0 0;
   display: flex;
   justify-content: space-between;
   align-items: center;
   background: ${(props) => props.theme.fondoblanco};
   color: ${(props) => props.theme.textonegro};
 
-  ${breakpoints.min_xl} {
-    padding: 100px 0;
+  ${breakpoints.min_desktop} {
+    padding: 144px 0 0;
   }
 `;
 
 export const Content = styled.div`
-  ${breakpoints.min_tablet} {
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Card = styled.div`
+  display: flex;
+  height: 200px;
+  width: calc(226px - (16px * 2));
+  padding: 16px;
+  flex-direction: column;
+  align-items: center;
+
+  ${breakpoints.min_desktop} {
+    width: 348px;
+    height: 376px;
+    padding: 32px;
+  }
+`;
+
+export const CardContainer = styled.div`
+  display: none;
+
+  ${breakpoints.min_desktop} {
     display: flex;
-    text-align: center;
     justify-content: space-between;
   }
-  ${breakpoints.min_tablet} {
-    & .corte-exacto-desktop-medio {
-      min-width: fit-content;
-    }
-  }
 `;
 
-export const Reverse = styled.div`
-  ${breakpoints.min_tablet} {
-    display: flex;
-    flex-direction: column-reverse;
-    margin-top: 40px;
-  }
-`;
-
-export const Areas = styled.div`
-  margin-top: 40px;
+export const SVGContainer = styled.div`
+  width: 40px;
+  height: 40px;
+  border: 2px solid ${(props) => props.theme.textonegro};
+  border-radius: 50%;
   display: flex;
-  justify-content: space-between;
+  align-items: center;
+  justify-content: center;
 
-  ${breakpoints.min_tablet} {
-    margin-top: 0px;
-    flex-wrap: wrap;
-    justify-content: center;
+  ${breakpoints.min_desktop} {
+    border: 3px solid ${(props) => props.theme.textonegro};
+    width: 80px;
+    height: 80px;
+  }
+`;
 
-    & p {
-      margin-bottom: 30px;
-      width: 50%;
-    }
+export const Scalability = styled.svg`
+  width: 16.539px;
+  height: 19.334px;
+
+  ${breakpoints.min_desktop} {
+    width: 33.057px;
+    height: 38.668px;
+  }
+`;
+
+export const Quality = styled.svg`
+  width: 21.656px;
+  height: 21.667px;
+
+  ${breakpoints.min_desktop} {
+    width: 43.313px;
+    height: 43.335px;
   }
 `;
